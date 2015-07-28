54d7b67d055994a542170db8fe56788cde55f0b6
Dimitris Akridas <d.akridas@gmail.com>
2015-07-28 12:42:40 +0300

    Example for function declarations

---
+++ grammar_examples/function_definitions_grammar.v
@@ -0,0 +1,13 @@
+module abc (id3, id4);
+    function automatic [63:0] factorial (input reg [31:0] n,t,r);
+    endfunction
+
+    function real multiply;
+        input a, b;
+    endfunction
+
+    function Parity_Calc_Func;
+         input [31:0] addr;
+    endfunction
+
+endmodule
