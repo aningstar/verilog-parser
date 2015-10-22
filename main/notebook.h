16835e40d9f6f43070460ade8cd09901722c3583
Dimitris Akridas <d.akridas@gmail.com>
2015-10-22 15:44:01 +0300

    Fix defintion of notebook_add_page to library

--- main/notebook.h
+++ main/notebook.h
@@ -5,4 +5,4 @@ GtkWidget *notebook_current_view();
 // creates a text view with the contents
 // of the given file and append it to the
 // notebook
-void notebook_add_page();
+void notebook_add_page(gchar *filename);
